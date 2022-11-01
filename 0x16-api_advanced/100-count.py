89df3f7ff2a07e0812ef82435ae83a377b880ff6
Tsion Moges <tsionmoges25@gmail.com>
2022-11-02 00:46:15 +0300

    100 count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,54 +1,55 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit."""
-import requests
+"""
+parses the title of all hot articles, and prints a sorted count of given
+keywords (case-insensitive, delimited by spaces)
+"""
+from collections import OrderedDict
+from requests import get
 
 
-def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
+def count_words(subreddit, word_list, after=None, match_dict={}):
+    """parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces)
     Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
+        subreddit (str): subreddit
+        word_list (list): list of words to count occurrences for
+        after (str): refer to next page
+        match_dict (dict): dictionary of frequency of words from word_list
+        flag (int): indicate when match_dict is complete
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
     try:
-        results = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
-        return
+        r = get('https://www.reddit.com/r/{}/hot.json?limit=100&&'
+                'after={}'.format(subreddit, after),
+                headers={'User-Agent': 'bc'})
+        sub_dict = r.json()
 
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
-        for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
+        if match_dict == {}:
+            for w in word_list:
+                match_dict[w] = 0
+
+        after = sub_dict['data']['after']
+
+        for i in range(len(sub_dict['data']['children'])):
+            title_string = sub_dict['data']['children'][i]['data']['title']
+            search_list = title_string.split()
+            for word in search_list:
+                for w in word_list:
+                    if w.lower() == word.lower():
+                        match_dict[w] += 1
+
+        if after is None:
+            descend_dict = OrderedDict(sorted(match_dict.items(),
+                                              key=lambda x: x[1],
+                                              reverse=True))
+            zero_count = 0
+            for k, v in descend_dict.items():
+                if v != 0:
+                    print("{}: {}".format(k, v))
                 else:
-                    instances[word] += times
-
-    if after is None:
-        if len(instances) == 0:
-            print("")
-            return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
-    else:
-        count_words(subreddit, word_list, instances, after, count)
+                    zero_count += 1
+            if zero_count == len(descend_dict):
+                print()
+        else:
+            count_words(subreddit, word_list, after, match_dict)
+    except:
+        pass
